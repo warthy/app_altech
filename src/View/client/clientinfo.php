74a153026e36b4b62dbb53c67d54dc5c561c9606
Lucie <lucie.deroo@isep.fr>
2020-04-19 17:08:39 +0200

    Client info view

---
+++ src/View/client/clientinfo.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link href="https://fonts.googleapis.com/css?family=Alegreya+Sans&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="/css/clientinfo.css" />
+        
+    </head>
+
+    <body>
+        <div class="numero-client">
+            <h1>Client :</h1>
+        </div>
+
+        <div class="infos-clients">
+            <div class="specifiques">
+            <h2 class="societe">Auto-écode de Paris</h2>
+                <p>Date d'inscription : </p>
+                <h3>Contacts de l'entreprise :</h3>
+                Adresse : <br>
+                Ville : <br>
+                Code Postal : <br>
+                Email : <br>
+                Téléphone : <br>
+                <br>
+                <!--<p>Nombre de candidats : </p>-->
+
+                <h3>Représentant légal :</h3>
+                NOM Prénom <br>
+                Email : <br>
+                Téléphone : <br>
+                <br>
+
+
+
+                <a href="#default" class="liste candidats">
+                    Consulter les candidats
+                </a>
+            </div>
+
+          <div class="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.239977941443!2d2.342671315807106!3d48.85363410899538!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e671e087bfe08b%3A0x2d75faebf854990e!2s19%20Quai%20Saint-Michel%2C%2075005%20Paris!5e0!3m2!1sfr!2sfr!4v1584539683732!5m2!1sfr!2sfr" width="300" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+          </div>  
+        </div> 
+    </body>
+</html>
